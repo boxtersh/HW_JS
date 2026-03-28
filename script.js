52a2e4503453f0d4a108afadefa4d04fc6345415
boxtersh <boxtersh@yandex.ru>
2026-03-28 20:27:44 +0300

    add code

--- script.js
+++ script.js
@@ -49,3 +49,59 @@ function btn_7() {
     document.getElementById('input_7').disabled = false;
 }
 
+let square = document.getElementById('square');
+square.addEventListener('mouseover', (event) => {
+    let heading = document.getElementById('heading');
+    heading.textContent = 'Наведено';
+});
+
+square.addEventListener('mouseout', (event) => {
+    let heading = document.getElementById('heading');
+    heading.textContent = '';
+});
+
+let btn = document.getElementById('btnTask5');
+let img = document.getElementById('imgTask5');
+btn.addEventListener('click', (event) => {
+    attributeImg = img.getAttribute("src");
+    value = attributeImg=="/image/img_1.png"? "/image/img_2.png":"/image/img_1.png";
+    img.setAttribute("src", value);
+});
+
+let input = document.getElementById('inputTask6');
+let btnTask6 = document.getElementById('btnTask6');
+let ul = document.getElementById('ulTask6');
+btnTask6.addEventListener('click', () => {
+    let value = input.value.trim();
+    if (value) {
+    let li = document.createElement('li');
+    li.textContent = value;
+    ul.append(li);
+    input.value = '';
+    }
+    else {alert('Вы не создали заметку');}
+});
+
+let inputTask7 = document.getElementById('inputTask7');
+let btnTask7 = document.getElementById('btnTask7');
+btnTask7.addEventListener('click', () => {
+    inputTask7.after(document.createElement('input'));
+    inputTask7.after(document.createElement('br'));
+    inputTask7.after(document.createElement('br'));
+});
+
+let btnTask8 = document.getElementById('btnTask8');
+let olTask8 = document.querySelector('#btnTask8+ol');
+btnTask8.addEventListener('click', () => {
+    if (olTask8.lastElementChild) {
+    olTask8.lastElementChild.remove();
+    }
+    btnTask8.disabled = !olTask8.lastElementChild;
+});
+
+let listLi = document.querySelectorAll('#ulTask9 li');
+listLi.forEach(elm => {
+    elm.addEventListener('click', function() {
+    this.remove();
+    });
+});
